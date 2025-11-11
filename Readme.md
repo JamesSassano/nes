5cf7eee9cdee8ffaa1b4301a59f094bce6eaeb44
✨𝕁𝕒𝕞𝕖𝕤 𝕊𝕒𝕤𝕤𝕒𝕟𝕠✨ <james.sassano@gmail.com>
2025-11-10 20:45:28 -0500

    Add Lego Hyrule

---
+++ Readme.md
@@ -0,0 +1,12 @@
+Download and unzip the LDraw library:
+
+```
+curl https://library.ldraw.org/library/updates/complete.zip
+unzip comlete.zip
+```
+
+Run a local web server:
+
+```
+npx serve
+```
---
+++ three/fetchThree.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+version=0.180.0
+for i in \
+    build/three.core.js \
+    build/three.module.js \
+    examples/jsm/controls/OrbitControls.js \
+    examples/jsm/loaders/LDrawLoader.js \
+    examples/jsm/materials/LDrawConditionalLineMaterial.js \
+    examples/jsm/utils/BufferGeometryUtils.js \
+; do 
+    mkdir -p "v$version/"`dirname $i`
+    curl "https://cdn.jsdelivr.net/npm/three@$version/$i" -o "v$version/$i"
+done
